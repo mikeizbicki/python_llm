41341b11c9443c85bbd31068395e3bc49f2b847b
Apple <apple@MacBook-Air-2988.local>
2026-04-08 20:23:13 -0700

    .

---
+++ tools/ls.py
@@ -0,0 +1,18 @@
+#step1: create a function with the rght numbr of args: write the doctstring
+#step2: create the doctests for that function 
+#step3: get the function to pass doctest 
+#has a lot that's not typed here, go to the zoom and type everything down
+
+import glob
+
+def ls(folder=None):
+    '''
+    This function bejaves just like the ls problems in the shell
+    '''
+    
+
+
+    
+
+
+for path in sorted(glob.glob(folder+'*/'))
